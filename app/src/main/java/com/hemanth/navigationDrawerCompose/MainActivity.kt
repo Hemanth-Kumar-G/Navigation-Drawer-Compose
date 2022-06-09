e12e3cdbf16531717f87f28b78226f20d4524e30
hemanth <hemanth@vyaparapp.in>
2022-06-09 11:04:03 +0530

    init

---
+++ app/src/main/java/com/hemanth/navigationDrawerCompose/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.hemanth.navigationDrawerCompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hemanth.navigationDrawerCompose.ui.theme.NavigationDrawerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationDrawerComposeTheme {
+
+            }
+        }
+    }
+}
